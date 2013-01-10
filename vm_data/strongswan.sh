5329d11d7b08ba5b97c4807fb84438668fd5fa74
Thomas Dixon <thom@thomdixon.org>
2013-01-10 14:18:46 -0500

    Cache strongswan svn repository to speed up build
    You can delete the vm_data/strongswan_styx to force it to re-checkout
    the repo.

--- vm_data/strongswan.sh
+++ vm_data/strongswan.sh
@@ -4,20 +4,19 @@ STRONGSWAN_SH=yes
 . "$(pwd)"/aptitude.sh
 . "$(pwd)"/backports.sh
 
+SS_URL=svn://svn.forge.objectweb.org/svnroot/contrail/trunk/resource/strongswan
+
 apti subversion libjansson-dev build-essential automake libtool libgmp3-dev \
      libsoup2.4-dev bison flex gperf libreadline-dev
 
-TEMP_DIR="$(mktemp -d)"
-cd "$TEMP_DIR"
-svn checkout \
-    svn://svn.forge.objectweb.org/svnroot/contrail/trunk/resource/strongswan \
-    strongswan_styx
-cd -; cd "$TEMP_DIR/strongswan_styx/strongswan"
+if test ! -d "strongswan_styx"; then
+    svn checkout $SS_URL strongswan_styx
+fi
+cd "strongswan_styx/strongswan"
 ./autogen.sh
 ./configure --prefix=/usr --sysconfdir=/etc --enable-styx
 make
 make install
 cd -
-rm -rf "$TEMP_DIR"
 
 fi #STRONGSWAN_SH
