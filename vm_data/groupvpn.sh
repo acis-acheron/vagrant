1d00cac44e5e70cbc13ef35ddd21eb62d1a25ded
Benjamin Woodruff <benjaminwoodruff@ufl.edu>
2013-01-10 13:21:56 -0500

    Attempts to fix groupvpn's messed up resolv.conf
    This is done by backing up the old resolv.conf file, and intelligently
    appending it back after installing groupvpn

--- vm_data/groupvpn.sh
+++ vm_data/groupvpn.sh
@@ -1,7 +1,13 @@
 if test -z "$GROUPVPN_SH"; then
 GROUPVPN_SH=yes
 
+# Backup the resolv.conf file which groupvpn replaces
+cp /etc/resolv.conf /etc/resolv.conf.bak
+
 # Use the script from our documents repository
 ./install_groupvpn.sh demo-groupvpn-config.zip
+
+# Fix the resolv.conf from our backup, use resolvconf
+resolvconf -a eth0 < /etc/resolv.conf.bak
 
 fi # $GROUPVPN_SH
