17fc5ad880c34b6dd97602e2c05851fcb94115b9
Benjamin Woodruff <benjaminwoodruff@ufl.edu>
2013-01-10 13:24:16 -0500

    Disables installation of fancy shell features
    You can re-enable them by uncommenting the relevant line, but they slow
    down the vagrant build process.

--- vm_data/main.sh
+++ vm_data/main.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-. "$(pwd)"/shell_features.sh # (optional) make ssh friendlier
+#. "$(pwd)"/shell_features.sh # (optional) make ssh friendlier
 . "$(pwd)"/strongswan.sh # build and install strongswan (don't configure)
 # Installing groupvpn breaks networking?
 . "$(pwd)"/groupvpn.sh # install and configure groupvpn
