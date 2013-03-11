07f40d408c60866bcdb05e30f19c4caddecfb702
Thom Dixon <thom@thomdixon.org>
2013-03-11 18:30:59 -0400

    Updated IPOP version
    Use a newly updated version of IPOP which eliminates a race
    condition caused by IPOP broadcasting the connecting IPs to both
    endpoints. Moreover, we now call update-rc.d in postinst, so it's
    no longer necessary to do so within the installation script.

--- vm_data/groupvpn.sh
+++ vm_data/groupvpn.sh
@@ -8,24 +8,7 @@ cp /etc/resolv.conf /etc/resolv.conf.bak
 
 apti mono-complete resolvconf
 
-RESULT=1
-while [ $RESULT -ne 0 ]
-do 
-  # Force the installation of the ipop package
-  if test ! -e "ipop-squeeze.deb.tar"; then
-    tar cvf ipop-squeeze.deb.tar ipop-squeeze.deb
-  fi
-
-  # Hacktastic Mr. Hacky Hackerson.
-  TEMP_DIR="$(mktemp -d)"
-  cp ipop-squeeze.deb.tar "$TEMP_DIR"
-  cd "$TEMP_DIR"
-  tar xvf ipop-squeeze.deb.tar
-  dpkg -i --force-depends ipop-squeeze.deb
-  RESULT=$?
-  cd -
-  rm -rf "$TEMP_DIR"
-done
+dpkg -i --force-depends ipop-squeeze.deb
 
 # Use apt-get which will opt to install the unmet
 # dependencies by default (whereas aptitude will
@@ -33,7 +16,6 @@ done
 apt-get install -fy
 
 groupvpn_prepare.sh demo-groupvpn-config.zip
-update-rc.d groupvpn.sh defaults # groupvpn should really do this
 /etc/init.d/groupvpn.sh start
 
 # Fix the resolv.conf from our backup, use resolvconf
